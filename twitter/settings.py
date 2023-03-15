a3a57aee1eff3f9b397f839f2e149b8362c156b1
liuminming <liuminming1989@gmail.com>
2023-03-14 22:01:30 -0400

    install django debug tool

--- twitter/settings.py
+++ twitter/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = '2i7&o+&n!)d9kf2ant6%ipkfx2r&40a^is+=8lt(n3f(0pjmal'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.33.10', 'localhost']
+INTERNAL_IPS = ['10.0.2.2']
 
 
 # Application definition
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'debug_toolbar'
 ]
 
 REST_FRAMEWORK = {
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = 'twitter.urls'
