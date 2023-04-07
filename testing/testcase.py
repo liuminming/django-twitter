a8057a5844b1457103224a9d8f96c4e7d652d6ba
liuminming <liuminming1989@gmail.com>
2023-04-06 21:02:04 -0400

    Add comment model and admin

--- testing/testcase.py
+++ testing/testcase.py
@@ -2,6 +2,7 @@ from django.test import TestCase as DjangoTestCase
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
 
+from comments.models import Comment
 from tweets.models import Tweet
 
 class TestCase(DjangoTestCase):
@@ -26,3 +27,8 @@ class TestCase(DjangoTestCase):
         if content is None:
             content = 'default tweet content'
         return Tweet.objects.create(user=user, content = content)
+
+    def create_comment(self, user, tweet, content=None):
+        if content is None:
+            content = 'default comment content'
+        return Comment.objects.create(user=user, tweet=tweet, content=content)
--- twitter/settings.py
+++ twitter/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'tweets',
     'friendships',
-    'newsfeeds'
+    'newsfeeds',
+    'comments',
 ]
 
 REST_FRAMEWORK = {
