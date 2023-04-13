ac205ddc2f76a52c7ecf3841e9da610522d3d074
liuminming <liuminming1989@gmail.com>
2023-04-12 21:06:44 -0400

    Comments list api

--- comments/api/views.py
+++ comments/api/views.py
@@ -11,6 +11,7 @@ class CommentViewSet(viewsets.GenericViewSet):
 
     serializer_class = CommentSerializerForCreate
     queryset = Comment.objects.all()
+    filterset_fields = ('tweet_id', )
 
     def get_permissions(self):
         if self.action == 'create':
@@ -18,6 +19,20 @@ class CommentViewSet(viewsets.GenericViewSet):
         if self.action in ['update', 'destroy']:
             return [IsAuthenticated(), IsObjectOwner()]
         return [AllowAny()]
+
+    def list(self, request, *args, **kwargs):
+         if 'tweet_id' not in request.query_params:
+             return Response({
+                 'message': 'missing tweet_id in request',
+                 'success':False
+             }, status=status.HTTP_400_BAD_REQUEST)
+         queryset = self.get_queryset()
+         # comments = self.filter_queryset(queryset).order_by('created_at')
+         # optimize performance
+         comments = self.filter_queryset(queryset).prefetch_related('user').order_by('created_at')
+         serializer = CommentSerializer(comments, many=True)
+         return Response({'comments': serializer.data}, status=status.HTTP_200_OK)
+
 
     def create(self, request, *args, **kwargs):
         data = {
