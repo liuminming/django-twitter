cbdf47dc709e6785dabfe1a37d844b971b0e9237
liuminming <liuminming1989@gmail.com>
2023-04-17 23:38:40 -0400

    Add likes cancel api

--- likes/api/serializers.py
+++ likes/api/serializers.py
@@ -13,7 +13,7 @@ class LikeSerializer(serializers.ModelSerializer):
         model = Like
         fields = ('user', 'created_at')
 
-class LikeSerializerForCreate(serializers.ModelSerializer):
+class LikeSerializerForCreateAndCancelBase(serializers.ModelSerializer):
     content_type = serializers.ChoiceField(choices=['comment', 'tweet'])
     object_id = serializers.IntegerField()
 
@@ -36,6 +36,7 @@ class LikeSerializerForCreate(serializers.ModelSerializer):
             raise ValidationError({'object_id': 'Object does not exist'})
         return data
 
+class LikeSerializerForCreate(LikeSerializerForCreateAndCancelBase):
     def create(self, validated_data):
         model_class = self.get_model_class(validated_data)
         instance, _ = Like.objects.get_or_create(
@@ -44,3 +45,12 @@ class LikeSerializerForCreate(serializers.ModelSerializer):
             user=self.context['request'].user,
         )
         return instance
+class LikeSerializerForCancel(LikeSerializerForCreateAndCancelBase):
+    def cancel(self):
+        model_class = self.get_model_class(self.validated_data)
+        Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(model_class),
+            object_id=self.validated_data['object_id'],
+            user=self.context['request'].user,
+        ).delete()
+
