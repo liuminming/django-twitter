1513707f491c5db5bc992ca2a6278bec04741aa5
liuminming <liuminming1989@gmail.com>
2023-05-15 15:59:57 -0400

    Add pagination to friendships

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -10,29 +10,28 @@ from friendships.api.serializers import (
 )
 from django.contrib.auth.models import User
 
+from utils.paginations import FriendshipPagination
+
 
 class FriendshipViewSet(viewsets.GenericViewSet):
     serializer_class = FriendshipSerializerForCreate
     queryset = User.objects.all()
+    pagination_class = FriendshipPagination
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followers(self, request, pk):
         friendships = Friendship.objects.filter(to_user_id=pk).order_by('-created_at')
-        serializer = FollowerSerializer(friendships, many=True)
-        return Response(
-            {'followers': serializer.data},
-            status=status.HTTP_200_OK
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowerSerializer(page, many=True, context = {'request':request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followings(self, request, pk):
         friendships = Friendship.objects.filter(from_user_id=pk).order_by('-created_at')
-        serializer = FollowingSerializer(friendships, many=True)
+        page = self.paginate_queryset(friendships)
+        serializer = FollowingSerializer(friendships, many=True, context = {'request':request})
+        return self.get_paginated_response(serializer.data)
 
-        return Response(
-            {'followings':serializer.data},
-            status=status.HTTP_200_OK
-        )
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
         # check if user with id=pk exists
--- friendships/services.py
+++ friendships/services.py
@@ -31,3 +31,11 @@ class FriendshipService(object):
             to_user=user,
         ).prefetch_related('from_user')
         return [friendship.from_user for friendship in friendships]
+
+    @classmethod
+    def has_followed(self, from_user, to_user):
+        return Friendship.objects.filter(
+            from_user=from_user,
+            to_user=to_user
+        ).exists()
+
