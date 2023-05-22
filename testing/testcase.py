ef07c2b5f3be95d91a6683db252f20334ff0efdd
liuminming <liuminming1989@gmail.com>
2023-05-22 19:41:46 -0400

    add redis as cache

--- testing/testcase.py
+++ testing/testcase.py
@@ -7,11 +7,14 @@ from comments.models import Comment
 from likes.models import Like
 from newsfeeds.models import NewsFeed
 from tweets.models import Tweet
+from utils.redis_client import RedisClient
+
 
 class TestCase(DjangoTestCase):
 
     def clear_cache(self):
         caches['testing'].clear()
+        RedisClient.clear()
 
     @property
     def anonymous_client(self):
