0697d08ed83dbe864a5e9fa57e922d1ce7780a41
liuminming <liuminming1989@gmail.com>
2023-05-23 19:00:33 -0400

    Cache tweet list in redis

--- tweets/tests.py
+++ tweets/tests.py
@@ -4,6 +4,8 @@ from tweets.constants import TweetPhotoStatus
 from tweets.models import Tweet, TweetPhoto
 from datetime import timedelta
 
+from tweets.services import TweetService
+from twitter.cache import USER_TWEETS_PATTERN
 from utils.redis_client import RedisClient
 from utils.redis_serializer import DjangoModelSerializer
 from utils.time_helpers import utc_now
@@ -55,3 +57,47 @@ class TweetTests(TestCase):
         cached_tweet = DjangoModelSerializer.deserialize(data)
         # assertEqual will compare content, even their in memory addresses are different
         self.assertEqual(tweet, cached_tweet)
+
+class TweetServiceTests(TestCase):
+
+    def setUp(self):
+        self.clear_cache()
+        self.alice = self.create_user('alice')
+
+    def test_get_user_tweets(self):
+        tweet_ids = []
+        for i in range(3):
+            tweet = self.create_tweet(self.alice, 'tweet {}'.format(i))
+            tweet_ids.append(tweet.id)
+        tweet_ids = tweet_ids[::-1]
+
+        RedisClient.clear()
+        conn = RedisClient.get_connection()
+
+        # cache miss
+        tweets = TweetService.get_cached_tweets(self.alice.id)
+        self.assertEqual([t.id for t in tweets], tweet_ids)
+
+        # cache hit
+        tweets = TweetService.get_cached_tweets(self.alice.id)
+        self.assertEqual([t.id for t in tweets], tweet_ids)
+
+        # cache updated
+        new_tweet = self.create_tweet(self.alice, 'new tweet')
+        tweets = TweetService.get_cached_tweets(self.alice.id)
+        tweet_ids.insert(0, new_tweet.id)
+        self.assertEqual([t.id for t in tweets], tweet_ids)
+
+    def test_create_new_tweet_before_get_cached_tweets(self):
+        tweet1 = self.create_tweet(self.alice, 'tweet1')
+
+        RedisClient.clear()
+        conn = RedisClient.get_connection()
+
+        key = USER_TWEETS_PATTERN.format(user_id=self.alice.id)
+        self.assertEqual(conn.exists(key), False)
+        tweet2 = self.create_tweet(self.alice, 'tweet2')
+        self.assertEqual(conn.exists(key), True)
+
+        tweets = TweetService.get_cached_tweets(self.alice.id)
+        self.assertEqual([t.id for t in tweets], [tweet2.id, tweet1.id])
---
+++ utils/redis_helper.py
@@ -0,0 +1,54 @@
+from django.conf import settings
+
+from utils.redis_client import RedisClient
+from utils.redis_serializer import DjangoModelSerializer
+
+
+class RedisHelper:
+
+    @classmethod
+    def _load_objects_to_cache(cls, key, objects):
+        conn = RedisClient.get_connection()
+
+        serialized_list = []
+        for obj in objects:
+            serialized_data = DjangoModelSerializer.serialize(obj)
+            serialized_list.append(serialized_data)
+
+        if serialized_list:
+            # the following is like conn.rpush(key, 1,2,3)
+            # every rpush is a redis server request
+            conn.rpush(key, *serialized_list)
+            conn.expire(key, settings.REDIS_KEY_EXPIRE_TIME)
+
+    @classmethod
+    def load_objects(cls, key, queryset):
+        conn = RedisClient.get_connection()
+
+        if conn.exists(key):
+            serialized_list = conn.lrange(key, 0, -1)
+            objects = []
+            for serialized_data in serialized_list:
+                deserialized_obj = DjangoModelSerializer.deserialize(serialized_data)
+                objects.append(deserialized_obj)
+            return objects
+
+        cls._load_objects_to_cache(key, queryset)
+
+
+        return list(queryset)
+
+
+    @classmethod
+    def push_object(cls, key, obj, queryset):
+        conn = RedisClient.get_connection()
+        # 先检测在不在， 不然expire之后，直接push，会丢数据
+        if not conn.exists(key):
+            # 如果 key 不存在，直接从数据库里 load
+            # 就不走单个 push 的方式加到 cache 里了
+            cls._load_objects_to_cache(key, queryset)
+            return
+        serialized_data = DjangoModelSerializer.serialize(obj)
+        #因为是最新的tweet, 应该放在左边第一个
+        conn.lpush(key, serialized_data)
+
